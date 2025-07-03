79fe925443915bca43f367ef3d17e3db5e2b77b9
Prasad Kandregula <yournewemail@example.com>
2025-07-03 20:04:43 +0530

    toast messages

--- src/app/Home/pages/driver-registration/driver-registration.page.ts
+++ src/app/Home/pages/driver-registration/driver-registration.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-driver-registration',
@@ -14,16 +15,27 @@ export class DriverRegistrationPage implements OnInit {
     truckType: '',
     licenseNumber: '',
   };
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {
   }
 registerDriver() {
   // Only proceed if form is valid
   if (this.driver.fullName && this.driver.ownerName && this.driver.phone && this.driver.truckType && this.driver.licenseNumber) {
+     this.presentToast('Driver Registration successful!', 'success');
     console.log('Registering driver:', this.driver);
     // your API call or logic here
   }
+}
+
+async presentToast(message: string, color: string = 'success') {
+  const toast = await this.toastController.create({
+    message,
+    duration: 3000,           // visible for 3 seconds
+    color,                    // 'success', 'danger', 'warning', etc.
+    position: 'bottom'
+  });
+  await toast.present();
 }
 
 }
--- src/app/Home/pages/owner-login/owner-login.page.ts
+++ src/app/Home/pages/owner-login/owner-login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-owner-login',
@@ -11,16 +12,25 @@ import { Router } from '@angular/router';
 export class OwnerLoginPage implements OnInit {
   username = '';
   password = '';
-  constructor(private router: Router) { }
+  constructor(private router: Router, private toastController: ToastController) { }
 
   ngOnInit() {
   }
    login() {
-    if (this.username === 'owner' && this.password === 'hatchery123') {
+    if (this.username && this.password ) {
+      this.presentToast("Login Successful!", 'success')
       this.router.navigate(['/tabs']);
     } else {
       alert('Invalid credentials');
     }
   }
-
+async presentToast(message: string, color: string = 'success') {
+  const toast = await this.toastController.create({
+    message,
+    duration: 3000,           // visible for 3 seconds
+    color,                    // 'success', 'danger', 'warning', etc.
+    position: 'bottom'
+  });
+  await toast.present();
+}
 }
