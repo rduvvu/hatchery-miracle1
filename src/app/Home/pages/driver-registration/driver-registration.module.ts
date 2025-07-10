abbb765c7ec2e0b6abec512a5ddaca48613f1042
nistala <snistala@miraclesoft.com>
2025-07-10 17:28:13 +0530

    Form validation Added

--- src/app/Home/pages/driver-registration/driver-registration.module.ts
+++ src/app/Home/pages/driver-registration/driver-registration.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,7 +12,7 @@ import { DriverRegistrationPage } from './driver-registration.page';
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
+    IonicModule,ReactiveFormsModule,
     DriverRegistrationPageRoutingModule
   ],
   declarations: [DriverRegistrationPage]
